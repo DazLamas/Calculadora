d5d6182f03ebe3cae9760aa5c47e036fc9df95cd
Sarah Díaz Lamas <Sara@MBOOK17-2.local>
2014-10-05 22:36:24 +0200

    Calculator OK. DONE

--- script.js
+++ script.js
@@ -24,10 +24,10 @@ function multiply(){
 	}
 }
 function division(){
-	var number1 = parseInt(document.getElementById("numberInput1").value());
-	var number2 = parseInt(document.getElementById("numberInput2").value());
+	var number1 = parseInt(document.getElementById("numberInput1").value);
+	var number2 = parseInt(document.getElementById("numberInput2").value);
 	if (number1 != NaN && number2 != NaN){
 	//Are numbers
-		document.getElementsById("result")[0].innerHTML = number1/number2;
+		document.getElementsByClassName("result")[0].innerHTML = number1/number2;
 	}
 }
