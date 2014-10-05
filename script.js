e978110465272584a93cba9995488f128bb72dcc
Sarah Díaz Lamas <Sara@MBOOK17-2.local>
2014-10-05 21:26:50 +0200

    Substract OK

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function add(){
 	console.log(number1);
-	var number1 = document.getElementById("numberInput1").value;
-	var number2 = document.getElementById("numberInput2").value;
+	var number1 = parseInt(document.getElementById("numberInput1").value);
+	var number2 = parseInt(document.getElementById("numberInput2").value);
 	if (number1 != NaN && number2 != NaN){
 	//Are numbers
 		document.getElementsByClassName("result")[0].innerHTML = number1+number2;
@@ -10,7 +10,7 @@ function add(){
 function substract(){
 	var number1 = document.getElementById("numberInput1").value;
 	var number2 = document.getElementById("numberInput2").value;
-	if (number1 != NaN && number2 != NaN){
+	if (number1 = NaN && number2 = NaN){
 	//Are numbers
 		document.getElementsByClassName("result")[0].innerHTML = number1-number2;
 	}
