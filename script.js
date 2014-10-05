cf16594de76e35a0a553911e0162c93882915d61
Sarah Díaz Lamas <Sara@MBOOK17-2.local>
2014-10-05 21:46:51 +0200

    Add OK. Solved with parseFloat.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function add(){
 	console.log(number1);
-	var number1 = parseInt(document.getElementById("numberInput1").value);
-	var number2 = parseInt(document.getElementById("numberInput2").value);
+	var number1 = parseFloat(document.getElementById("numberInput1").value);
+	var number2 = parseFloat(document.getElementById("numberInput2").value);
 	if (number1 != NaN && number2 != NaN){
 	//Are numbers
 		document.getElementsByClassName("result")[0].innerHTML = number1+number2;
@@ -10,22 +10,22 @@ function add(){
 function substract(){
 	var number1 = document.getElementById("numberInput1").value;
 	var number2 = document.getElementById("numberInput2").value;
-	if (number1 = NaN && number2 = NaN){
+	if (number1 != NaN && number2 != NaN){
 	//Are numbers
 		document.getElementsByClassName("result")[0].innerHTML = number1-number2;
 	}
 }
 function multiply(){
-	var number1 = parseInt(document.getElementById("numberInput1").value);
-	var number2 = parseInt(document.getElementById("numberInput2").value);
+	var number1 = parseInt(document.getElementById("numberInput1").value());
+	var number2 = parseInt(document.getElementById("numberInput2").value());
 	if (number1 != NaN && number2 != NaN){
 	//Are numbers
 		document.getElementsById("result")[0].innerHTML = number1*number2;
 	}
 }
 function division(){
-	var number1 = parseInt(document.getElementById("numberInput1").value);
-	var number2 = parseInt(document.getElementById("numberInput2").value);
+	var number1 = parseInt(document.getElementById("numberInput1").value());
+	var number2 = parseInt(document.getElementById("numberInput2").value());
 	if (number1 != NaN && number2 != NaN){
 	//Are numbers
 		document.getElementsById("result")[0].innerHTML = number1/number2;
